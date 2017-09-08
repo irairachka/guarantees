5c933c2a9f5838e66d152a8393608a5581362af6
guywaburg <guy.warburg@gmail.com>
2017-09-08 17:36:17 +0300

    fix wizard display

---
+++ TruffleGUI/src/app/components/wizard/wizard.component.ts
@@ -0,0 +1,10 @@
+import {Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-wizard',
+  templateUrl: './wizard.component.html',
+  styleUrls: ['./wizard.component.scss']
+})
+export class WizardComponent {
+  @Input() options: any[];
+}
