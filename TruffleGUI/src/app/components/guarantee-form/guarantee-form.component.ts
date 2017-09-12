834c0e02efcf02557bf65151722954dcdd5d8af4
dmitryshlymovich <shdmitry@gmail.com>
2017-09-12 08:07:19 +0300

    pre demo

--- TruffleGUI/src/app/components/guarantee-form/guarantee-form.component.ts
+++ TruffleGUI/src/app/components/guarantee-form/guarantee-form.component.ts
@@ -114,41 +114,4 @@ export class GuaranteeFormComponent implements OnInit, OnChanges {
     this.terminateReason = '';
     this.displayActions = this.testDisplayAction();
   }
-
-  changeRequest(type) {
-    console.log('this.data', this.data);
-    let eventData = {
-      type: type,
-      requestId: this.data.GRequestID,
-      guaranteeId: '',
-      details: '',
-      update: {}
-    };
-
-    switch (type) {
-      case 'withdrawal':
-        break;
-      case 'updateBank':
-        eventData.details = this.selectedRequestsStates;
-        break;
-      case 'accept':
-        break;
-      case 'reject':
-        eventData.details = this.cancelReason;
-        break;
-      case 'terminate':
-        eventData.details = this.terminateReason;
-        eventData.guaranteeId = this.data.GuaranteeID;
-        break;
-      case 'guaranteeUpdate':
-        eventData.guaranteeId = this.data.GuaranteeID;
-        eventData.update = {
-          date: this.newDate,
-          amount: this.newValue
-        };
-        break;
-    }
-
-    this.updateRequest.emit(eventData);
-}
 }
