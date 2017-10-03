6daead96e21b5b5f370c2164f29ea4ff9812ebf1
dmitryshlymovich <shdmitry@gmail.com>
2017-10-03 12:21:23 +0300

    contracts full update

--- contracts/GuaranteeRequestExtender.sol
+++ contracts/GuaranteeRequestExtender.sol
@@ -1,8 +1,9 @@
 pragma solidity ^0.4.13;
 
 import "./GuaranteeConst.sol";
+import "./Ownable.sol";
 
-contract GuaranteeRequestExtender is GuaranteeConst {
+contract GuaranteeRequestExtender is Ownable,GuaranteeConst {
 
 
     //***
@@ -11,38 +12,61 @@ contract GuaranteeRequestExtender is GuaranteeConst {
     //premissions modifier for bank functions
     modifier onlyBank() {
         if ( msg.sender != getBank() ) {
-            loga("###ERROR-not performd by BANK address",msg.sender);
+//            loga("###ERROR-not performd by BANK address",msg.sender);
             throw;
         }
-        loga("#pass BANK action check",msg.sender);
+//        loga("#pass BANK action check",msg.sender);
         _;
     }
 
     //premissions modifier for customer functions
     modifier onlyCustomer() {
         if ( msg.sender != getCustomer() ) {
-            loga("###ERROR-not performd by CUSTOMER address",msg.sender);
+//            loga("###ERROR-not performd by CUSTOMER address",msg.sender);
             throw;
         }
-        loga("#pass CUSTOMER action check",msg.sender);
+//        loga("#pass CUSTOMER action check",msg.sender);
         _;
     }
 
     //premissions modifier for beneficiary functions
     modifier onlyBeneficiary() {
         if ( msg.sender != getBeneficiary() ) {
-            loga("###ERROR-not performd by BENEFICIERY address",msg.sender);
+//            loga("###ERROR-not performd by BENEFICIERY address",msg.sender);
             throw;
         }
-        loga("#pass BENEFICIERY action check",msg.sender);
+//        loga("#pass BENEFICIERY action check",msg.sender);
         _;
+    }
+
+
+    //premissions modifier for beneficiary functions
+    modifier onlyRegulator() {
+        if ( msg.sender != regulator ) {
+//            loga("###ERROR-not performd by BENEFICIERY address",msg.sender);
+            throw;
+        }
+//        loga("#pass BENEFICIERY action check",msg.sender);
+        _;
+    }
+
+    address  regulator;
+    address  guarantee;
+    address  changeRequest;
+
+    function setRegulator() public
+    {
+        regulator=msg.sender;
     }
 
 
 
 
     //    function getId() constant returns (address);
-    function getCustomer() constant public returns (address);
+    function getCustomer() constant public returns (address)
+    {
+        return owner;
+    }
     function getBank() constant public returns (address);
     function getBeneficiary() constant public returns (address);
 
@@ -60,6 +84,9 @@ contract GuaranteeRequestExtender is GuaranteeConst {
     bytes32 _purpose,uint _amount,uint _startDate,uint _endDate,IndexType _indexType,uint _indexDate,RequestState _status);
     function getProposalIPFSHash() constant public returns (bytes _proposalIPFSHash);
 
+//    function checkRegulator() constant public returns (bool) {
+//        return (getEndDate()>now);
+//    }
     function getRequestState() public constant returns (RequestState);
 //    function getCommentsForStep(int step) constant public returns (string);
 //    function addCommentsForStep(int _step,string _commentline) public ;
@@ -69,10 +96,14 @@ contract GuaranteeRequestExtender is GuaranteeConst {
         return (getEndDate()>now);
     }
 
+    function isValid() constant public returns (bool) {
+        return ( getBank()!= address(0) && getBeneficiary()!= address(0));
+    }
+
     function submit(string comment) onlyCustomer public returns (bool result) ;
-    function termination(string comment) onlyBeneficiary public returns (bool result);
+    function terminate() onlyRegulator public returns (bool result);
     function reject(string comment) onlyBank public returns (bool result);
-    function accept(string comment,bytes _guaranteeIPFSHash) onlyBank public returns (bool result);
+    function accept(string comment) onlyBank public returns (bool result);
     function withdrawal(string comment) onlyCustomer public returns (bool result);
     function bankStateChange(string comment ,RequestState _newState) onlyBank public returns (bool result);
 
