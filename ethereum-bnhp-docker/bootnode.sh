53f01e3cfb2115bf9d8d203e080bf9defdcd1329
dmitryshlymovich <shdmitry@gmail.com>
2017-12-25 07:25:46 +0200

    auto version (pre delivery)

---
+++ ethereum-bnhp-docker/bootnode.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# Runs a bootnode as regular miner with no
+#
+# If there is a GEN_CHAIN_ID variable it is passed on to the container.
+#
+
+NODE_NAME=$1
+NODE_NAME=${NODE_NAME:-"ethereum-bootnode-static"}
+
+docker stop $NODE_NAME
+docker rm $NODE_NAME
+IMGVERSION="1.0"
+#$(head -n 1 .IMGVERSION)
+IMGVERSION=${IMGVERSION:-"latest"}
+IMGNAME="bnhp/ethereum-static"
+NET_ARG=
+GEN_ARG=
+USE_STATIC_NODE_ARG="-e USE_STATIC_NODE=false"
+[[ ! -z $PRIVATE_PORT ]] && PRIVATE_PORT=" -e PRIVATE_PORT=$PRIVATE_PORT"
+[[ ! -z $USE_STATIC_NODE ]] && USE_STATIC_NODE_ARG=" -e USE_STATIC_NODE=$USE_STATIC_NODE"
+[[ ! -z $GEN_CHAIN_ID ]] && NET_ARG="-e GEN_CHAIN_ID=$GEN_CHAIN_ID"
+[[ ! -z $GEN_ALLOC ]] && GEN_ARG="-e GEN_ALLOC=$GEN_ALLOC"
+[[ ! -z MINING_NODE ]]  && USE_MINING_NODE_ARG="-e MINING_NODE=$MINING_NODE"
+DATA_ROOT=${DATA_ROOT:-$(pwd)}
+docker run -d --name $NODE_NAME \
+    -v $DATA_ROOT/.bootnode:/opt/bootnode \
+    -e "RUN_BOOTNODE=true" \
+    $PRIVATE_PORT \
+    $USE_STATIC_NODE_ARG \
+    $USE_MINING_NODE_ARG \
+    $NET_ARG \
+    $GEN_ARG \
+    $IMGNAME:$IMGVERSION --verbosity=3
---
+++ ethereum-bnhp-docker/src/truffle-ins.sh
@@ -0,0 +1,59 @@
+set -xv
+#npm config set registry http://34.252.72.81:8081/artifactory/api/npm/npm-virtual/
+# npm update
+#npm update npm -g
+#npm install -g how-to-npm
+
+#truffle install
+
+npm install truffle -g
+npm install keythereum -g
+
+# webpack install
+git clone https://github.com/trufflesuite/truffle-init-webpack.git
+truffle unbox webpack
+
+
+# garanties intall
+#git clone https://github.com/shdmitry2000/guarantees.git /opt/guarantees
+#npm install forever forever-monitor -g
+#cd /opt/guarantees
+#npm install
+#truffle compile
+#cd /opt/guarantees/TruffleGUI/
+#npm install
+#npm run-script build --environment=service
+#cd /opt/guarantees/ExpressServer/
+#npm install
+#sudo git pull origin newUI
+##forever stop -c "npm start --env=service" ./
+##forever start -c "npm start --env=service" ./
+
+
+
+
+#install examples
+mkdir /opt/simple-storage
+cd /opt/simple-storage
+truffle init
+echo "pragma solidity ^0.4.17;
+
+      contract SimpleStorage {
+        uint myVariable;
+
+        function set(uint x) public {
+          myVariable = x;
+        }
+
+        function get() constant public returns (uint) {
+          return myVariable;
+        }
+      }
+"  > /opt/simple-storage/contracts/Store.sol
+
+echo  " var SimpleStorage = artifacts.require("SimpleStorage");
+
+     module.exports = function(deployer) {
+       deployer.deploy(SimpleStorage);
+     }; " > /opt/simple-storage/migrations/2_deploy_contracts.js
+
