30be69412f9348308a562664762693fbe3b1a8ff
dmitryshlymovich <shdmitry@gmail.com>
2018-01-25 15:31:27 +0200

    חברה לאטומציה

--- ethereum-bnhp-docker/getbootnodenodiscovery.sh
+++ ethereum-bnhp-docker/getbootnodenodiscovery.sh
@@ -2,5 +2,5 @@
 nodename="ethereum-bootnode-static"
 
 DOCKER_IP=$(docker inspect   ethereum-bootnode-static |  grep '"IPAddress"' | head -n 1 |awk -F  ": " '{print $2}' |  awk -F  '"' '{print $2}' | awk -F  '"' '{print $1}')
-ENODE_LINE=$(docker logs ethereum-bootnode-static 2>&1 |  grep self=enode | grep enode | head -n 1 | awk -F  "self=enode://" '{print $2}' | awk -F  '[::]' '{print $1$4} ' |  awk -F  [ '{print $1"'$DOCKER_IP':"$2} ' )
+ENODE_LINE=$(docker logs ethereum-bootnode-static 2>&1 |  grep enode:// | grep enode | head -n 1 | awk -F  "enode://" '{print $2}' | awk -F  '[::]' '{print $1$4} ' |  awk -F  [ '{print $1"'$DOCKER_IP':"$2} '   | awk -F  "?" '{print $1}'  )
 echo "enode://${ENODE_LINE#*enode:}"
